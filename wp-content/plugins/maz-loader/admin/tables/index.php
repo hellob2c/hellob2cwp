7e48a7ac56ee79dc906d7fd42f0ea2d43f0a4268
mkcmukesh <hellob2cindia@gmail.com>
2021-10-20 20:01:30 +0530

    ss

---
+++ wp-content/plugins/maz-loader/admin/tables/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Silence is golden
+ *
+ * @link   https://www.feataholic.com
+ * @author Feataholic
+ * @since  1.3.4 Free
+ *
+ * @package MZLDR/Admin/Tables
+ */
---
+++ wp-content/plugins/maz-loader/uninstall.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Fired when the plugin is uninstalled.
+ *
+ * @link       https://www.feataholic.com
+ * @since      1.3.4 Free
+ *
+ * @package    MZLDR
+ */
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
